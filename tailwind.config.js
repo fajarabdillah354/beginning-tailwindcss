60d8eb8a2450b7ea31753299efa2e5e9aaac45e5
fajar abdillah <fajardillah25@gmail.com>
2025-04-02 23:48:41 +0700

    Modify

---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+// jika extention tailwindcss intelligence tidak dapat memberikan suggest text maka kita perlu 
+// memberi tahu VS code bahwa ini adalah project tailwind dengan cara membuat class tailwind.config.js
+
+
+// pada tailwind versi 4 kita tidak perlu lagi menuliskan file tailwind.config.js 
